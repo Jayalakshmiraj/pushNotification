bd4a29e4ab0162547b7d88488bbcee239a1af387
Jayalakshmiraj <Jayalakshmiraj@gmail.com>
2019-05-14 14:35:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyAVbDx5GWyQJLYb6d5ghrKdPwV111EcCTg",
+  authDomain: "pushnotification-fec6b.firebaseapp.com",
+  databaseURL: "https://pushnotification-fec6b.firebaseio.com",
+  projectId: "pushnotification-fec6b",
+  storageBucket: "pushnotification-fec6b.appspot.com",
+  messagingSenderId: "566272227518"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    AngularFireModule , 
+    AngularFireModule.initializeApp(config),IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
